0255479bb16b98d17b311fe44526d0d69f619fb0
Aradhya2005 <73036479+Aradhya2005@users.noreply.github.com>
2020-12-30 21:45:50 -0800

    Update ElevensGUIRunner.java

--- Activity Starter Code/Activity9 Starter Code/ElevensGUIRunner.java
+++ Activity Starter Code/Activity9 Starter Code/ElevensGUIRunner.java
@@ -3,6 +3,7 @@
  * See accompanying documents for a description of how Elevens is played.
  */
 public class ElevensGUIRunner {
+	// The cards file is neccessary to see the output
 
 	/**
 	 * Plays the GUI version of Elevens.
